382972bbee02f3a06c59bb76dcef08704fdeda03
Vishal Goney <vishalgoney@Vishals-Laptop.lan1>
2024-07-16 16:53:03 -0400

    Initial commit

---
+++ sindhudjango/sindhu_api/admin.py
@@ -0,0 +1,5 @@
+# api/admin.py
+from django.contrib import admin
+from .models import Item
+
+admin.site.register(Item)
---
+++ sindhudjango/sindhu_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SindhuApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sindhu_api'
